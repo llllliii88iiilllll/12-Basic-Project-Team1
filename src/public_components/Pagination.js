7311a9ad619f64b09b8ba673ab631a6b74bea2c7
Hyesun Kim <cocoabearhot@naver.com>
2024-12-13 03:07:46 +0900

    feat: 정렬, 페이지네이션, 아이템 불러오기, 로고, 답변하기 링크 연결

--- src/public_components/Pagination.js
+++ src/public_components/Pagination.js
@@ -2,15 +2,72 @@ import styles from "./Pagination.module.css";
 import { ReactComponent as IcArrowRight } from "../assets/Icon/arrow_right.svg";
 import { ReactComponent as IcArrowLeft } from "../assets/Icon/arrow_left.svg";
 
-function Pagination() {
+function Pagination({
+  setCurrentPage,
+  handleLoad,
+  LIMIT,
+  currentPage,
+  totalPages,
+}) {
+  const btnRange = 5;
+  const startPage = Math.floor(((currentPage - 1) / btnRange) * btnRange + 1);
+  const endPage = Math.min(totalPages, startPage + btnRange - 1);
+
+  const goToPreviousSet = () => {
+    if (startPage > 1) {
+      const newStartPage = Math.max(1, startPage - btnRange);
+      const newEndPage = newStartPage + btnRange - 1;
+      setCurrentPage(newEndPage);
+      const offset = (newEndPage - 1) * LIMIT;
+      handleLoad({ offset, limit: LIMIT });
+    }
+  };
+
+  const goToNextSet = () => {
+    if (endPage < totalPages) {
+      const newStartPage = endPage + 1;
+      setCurrentPage(newStartPage);
+      const offset = (newStartPage - 1) * LIMIT;
+      handleLoad({ offset, limit: LIMIT });
+    }
+  };
+
+  const goToPage = (pageNumber) => {
+    setCurrentPage(pageNumber);
+    const offset = (pageNumber - 1) * LIMIT;
+    handleLoad({ offset, limit: LIMIT });
+  };
+
   return (
     <ul className={styles.pagination}>
-      <li className={styles.prev_button}>
+      <li
+        className={`${styles.prev_button} ${
+          startPage === 1 ? styles.disabled : ""
+        }`}
+        onClick={goToPreviousSet}
+      >
         <IcArrowLeft />
       </li>
-      <li className={styles.active}>1</li>
-      <li>2</li>
-      <li className={styles.next_button}>
+
+      {Array.from({ length: totalPages }, (_, idx) => {
+        const pageNumber = idx + 1;
+        return (
+          <li
+            key={pageNumber}
+            onClick={() => goToPage(pageNumber)}
+            className={currentPage === pageNumber ? styles.active : ""}
+          >
+            {pageNumber}
+          </li>
+        );
+      })}
+
+      <li
+        className={`${styles.next_button} ${
+          endPage >= totalPages ? styles.disabled : ""
+        }`}
+        onClick={goToNextSet}
+      >
         <IcArrowRight />
       </li>
     </ul>
--- src/public_components/Sort.js
+++ src/public_components/Sort.js
@@ -1,19 +1,55 @@
 import styles from "./Sort.module.css";
+import { useRef, useState, useEffect, use } from "react";
 import { ReactComponent as IcDown } from "../assets/Icon/arrow_down.svg";
 import { ReactComponent as IcUp } from "../assets/Icon/arrow_up.svg";
 
-function Sort() {
+function Sort({ setOrder }) {
+  const selectRef = useRef(null);
+  const [currentValue, setCurrentValue] = useState("최신순");
+  const [showOptions, setShowOptions] = useState(false);
+
+  const handleClickSelectValue = (e) => {
+    const sortValue = e.target.getAttribute("value");
+    const sortName = e.target.getAttribute("name");
+    setCurrentValue(sortName);
+    setOrder(sortValue);
+  };
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (selectRef.current && !selectRef.current.contains(event.target)) {
+        setShowOptions(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [selectRef]);
+
   return (
-    <div className={styles.sort_wrap}>
+    <div
+      className={styles.sort_wrap}
+      onClick={() => {
+        setShowOptions(!showOptions);
+      }}
+      ref={selectRef}
+    >
       <label className={styles.sort_label_active}>
-        이름순
+        최신순
         <IcDown className={styles.sort_default} />
         <IcUp className={styles.sort_active} />
       </label>
-      <ul>
-        <li>이름순</li>
-        <li>최신순</li>
-      </ul>
+      {showOptions && (
+        <ul>
+          <li value="createdAt" name="최신순" onClick={handleClickSelectValue}>
+            최신순
+          </li>
+          <li value="name" name="이름순" onClick={handleClickSelectValue}>
+            이름순
+          </li>
+        </ul>
+      )}
     </div>
   );
 }
