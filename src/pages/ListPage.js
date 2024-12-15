fc36ef43f99fa56a44ec6e8bb555d8523f2eeab0
Hyesun Kim <cocoabearhot@naver.com>
2024-12-15 23:54:15 +0900

    chore: 데이터 불러올때 리액트 스피너 사용

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getSubjects } from "../apis/GetSubjects";
+import { BeatLoader } from "react-spinners";
+import styles from "./ListPage.module.css";
+import useWindowSize from "../utils/useWindowSize";
 import UserCard from "../public_components/UserCard";
-import pagestyles from "./ListPage.module.css";
 import ButtonLightAnswer from "../public_components/ButtonLightAnswer";
 import Sort from "../public_components/Sort";
-import LogoImage from "../assets/Images/logo.svg";
-import useWindowSize from "../utils/useWindowSize";
 import Pagination from "../public_components/Pagination";
+import LogoImage from "../assets/Images/logo.svg";
 
 function ListPage() {
   const [order, setOrder] = useState("createdAt");
@@ -59,24 +60,23 @@ function ListPage() {
   }, [order, currentPage, limit]);
 
   return (
-    <div className={pagestyles.list_wrap}>
-      <nav className={pagestyles.list_top}>
+    <div className={styles.list_wrap}>
+      <nav className={styles.list_top}>
         <Link to="/">
-          <img
-            src={LogoImage}
-            alt="로고 이미지"
-            className={pagestyles.list_logo}
-          />
+          <img src={LogoImage} alt="로고 이미지" className={styles.list_logo} />
         </Link>
         <ButtonLightAnswer>답변하러 가기</ButtonLightAnswer>
       </nav>
-      <section className={pagestyles.list_body}>
-        <div className={pagestyles.list_body_top}>
+      <section className={styles.list_body}>
+        <div className={styles.list_body_top}>
           <h1>누구에게 질문할까요?</h1>
           <Sort setOrder={setOrder} />
         </div>
         {loading ? (
-          <p>데이터를 불러오고 있습니다.</p>
+          <div className={styles.loading}>
+            <BeatLoader color="#542F1A" />
+            <p>데이터를 불러오고 있습니다.</p>
+          </div>
         ) : (
           <>
             <UserCard items={sortedItems} />
