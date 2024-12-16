1d0f1f7a37025e36fdb0701a978ee84af0aac0dd
Hyesun Kim <cocoabearhot@naver.com>
2024-12-16 18:48:01 +0900

    chore: getsubjects.js sort 삭제

--- src/apis/GetSubjects.js
+++ src/apis/GetSubjects.js
@@ -1,7 +1,7 @@
 import { BASE_URL } from "./ApiConfig";
 
-export async function getSubjects({ limit, offset, sort }) {
-  const query = `limit=${limit}&offset=${offset}&sort=${sort}`;
+export async function getSubjects({ limit, offset }) {
+  const query = `limit=${limit}&offset=${offset}`;
   const response = await fetch(`${BASE_URL}/subjects/?${query}`);
   if (!response.ok) {
     throw new Error("데이터를 불러오는데 실패했습니다.");
